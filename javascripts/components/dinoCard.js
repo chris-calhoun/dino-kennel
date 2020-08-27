a0e796ba541071a438cdd997ce92ec818e621ac9
Chris Calhoun <ctcalhoun2@gmail.com>
2020-08-27 17:11:37 -0500

    added ability to delete dino cards from DOM

--- javascripts/components/dinoCard.js
+++ javascripts/components/dinoCard.js
@@ -1,25 +1,23 @@
-import { getDinos } from '../helpers/data/dinoData.js';
-
 const createDinoCards = dinoArray => {
   const newestDino = dinoArray[dinoArray.length - 1];
-  console.log(newestDino.id);
-  $('#dinoKennel').append(`<div class="card" style="width: 16rem; ">
-      <img class="card-img-top" src=https://i.pinimg.com/originals/60/72/b9/6072b96192d18a2167afeb64371e769f.jpg alt="Card image cap">
-      <div class="card-body" >
-        <h5 class="card-title text-center"> ${newestDino.name}</h5>
-      </div>
-      <button type="button" class="btn btn-danger" id=${newestDino.id}>Delete</button>
-    </div>
+  $('#dinoKennel').append(`
+        <div class="card" id=${newestDino.id} style="width: 16rem; ">
+          <img class="card-img-top" src=https://i.pinimg.com/originals/60/72/b9/6072b96192d18a2167afeb64371e769f.jpg alt="Card image cap">
+          <div class="card-body" >
+            <h5 class="card-title text-center"> ${newestDino.name}</h5>
+          </div>
+          <button type="button" class="btn btn-danger" id=${newestDino.id}>Delete</button>
+        </div>
       `);
 };
 
-const deleteDino = e => {
-  const target = e.target.id;
-  const targetType = e.target.type;
-  if (targetType === 'button') {
-    getDinos().splice(target, 1);
-    createDinoCards();
-  }
+const deleteDino = () => {
+  $('#dinoLocation').on('click', e => {
+    const target = e.target.id;
+    if (e.target.innerHTML === 'Delete') {
+      $(`#${target}`).remove();
+    }
+  });
 };
 
 export { createDinoCards, deleteDino };
