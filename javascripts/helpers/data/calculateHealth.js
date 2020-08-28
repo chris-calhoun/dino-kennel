f90b4f0250fea139882fe42d07ab78d53a6563cb
Chris Calhoun <ctcalhoun2@gmail.com>
2020-08-28 12:41:11 -0500

    updated health of a dino based on action button clicked.

--- javascripts/helpers/data/calculateHealth.js
+++ javascripts/helpers/data/calculateHealth.js
@@ -3,12 +3,13 @@ import { getSelectedDino } from './dinoData.js';
 const calcHealth = target => {
   const arr = target.split('-');
   const id = arr[1];
+  const idToNum = parseInt(id, 10);
   if (target.includes('feed')) {
-    const feedId = target.slice(4, target.length);
-    const idToNum = parseInt(feedId, 10);
-    console.log(getSelectedDino(idToNum).health + 25);
+    getSelectedDino(idToNum).health += 25;
   } else if (target.includes('adventure')) {
+    getSelectedDino(idToNum).health -= 30;
   } else if (target.includes('pet')) {
+    getSelectedDino(idToNum).health += 10;
   }
 };
 
