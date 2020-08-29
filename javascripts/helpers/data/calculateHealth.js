e1cc6753ddac236ca38f64886905f94bccdce11c
Chris Calhoun <ctcalhoun2@gmail.com>
2020-08-29 10:42:08 -0500

    DRYed up code by adding more functionality to the getSelectedDino function.

--- javascripts/helpers/data/calculateHealth.js
+++ javascripts/helpers/data/calculateHealth.js
@@ -1,22 +1,19 @@
 import { getSelectedDino } from './dinoData.js';
 
 const calcHealth = target => {
-  const arr = target.split('-');
-  const id = arr[1];
-  const idToNum = parseInt(id, 10);
   if (target.includes('feed')) {
-    getSelectedDino(idToNum).health += 25;
+    getSelectedDino(target).health += 25;
   } else if (target.includes('adventure')) {
-    getSelectedDino(idToNum).health -= 30;
+    getSelectedDino(target).health -= 30;
   } else if (target.includes('pet')) {
-    getSelectedDino(idToNum).health += 10;
+    getSelectedDino(target).health += 10;
   }
 
   // Health max and min limits
-  if (getSelectedDino(idToNum).health > 100) {
-    getSelectedDino(idToNum).health = 100;
-  } else if (getSelectedDino(idToNum).health < 0) {
-    getSelectedDino(idToNum).health = 0;
+  if (getSelectedDino(target).health > 100) {
+    getSelectedDino(target).health = 100;
+  } else if (getSelectedDino(target).health < 0) {
+    getSelectedDino(target).health = 0;
   }
 };
 
--- javascripts/helpers/data/dinoData.js
+++ javascripts/helpers/data/dinoData.js
@@ -4,8 +4,11 @@ const getDinos = () => {
   return dinos;
 };
 
-const getSelectedDino = dino => {
-  return dinos[dino - 1];
+const getSelectedDino = target => {
+  const arr = target.split('-');
+  const id = arr[1];
+  const idToNum = parseInt(id, 10);
+  return dinos[idToNum - 1];
 };
 
 export { getDinos, getSelectedDino };
